f2e39f0b0c730c075c3a491aaba579cfc332ba94
CodyBanks <codydouglasbanks@gmail.com>
2020-06-15 13:38:53 -0400

    added window prompt for name

--- public/main.js
+++ public/main.js
@@ -1,6 +1,9 @@
 const numberOfCupsOfCoffee = 1
-const fullName = 'Cody'
+const fullName = 'Cody Banks'
 
 console.log(
   `Hello, my name is ${fullName}, and I drink ${numberOfCupsOfCoffee} cup of coffee a day!`
 )
+
+const userName = window.prompt('What is your name?')
+console.log(`It's a pleasure to meet you ${userName}!`)
