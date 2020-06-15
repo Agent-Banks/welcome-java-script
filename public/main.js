819e563242cf52c1a0fb919bdad3c5961b09bff8
CodyBanks <codydouglasbanks@gmail.com>
2020-06-15 13:20:29 -0400

    initial commit

--- public/main.js
+++ public/main.js
@@ -1,7 +1,2 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
-}
-
-document.addEventListener('DOMContentLoaded', main)
+const numberOfCupsOfCoffee = 1
+const fullName = 'Cody'
